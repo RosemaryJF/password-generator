377c4a7e865b961b9fe93f9745c26c086b528c95
RJ Dust Puppies <109449181+RosemaryJF@users.noreply.github.com>
2022-08-14 13:36:18 +1000

    Discovery of inputevent to progress to next stage of password generation

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,12 @@ var generateBtn = document.querySelector("#generate");
 var passwordLength = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 20];
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"];
 var numericCharacters = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-var specialCharacters = ["!", "@", "#", '"', "$", "%", ]
+var specialCharacters = ["!", "@", "#", '"', "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", "?"]
 
 console.log(passwordLength);
 console.log(letters);
 console.log(numericCharacters);
-console.log(specialCharacters);
-console.log(letters.toUppercase);   
+console.log(specialCharacters); 
 
 // Write password to the #password input
 function writePassword() {
@@ -23,14 +22,13 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
-  {window.confirm("Do you want to create a new password?");
+  window.confirm("Do you want to create a new password?");
+  
+  if 
+    (window.prompt("How many charactors should it to contain?")); 
 
   if 
-(window.prompt("How many charactors should it to contain?")); 
-
-if 
-(includes.passwordLength(window.prompt("Do you want to include lowercase letters?")));
-else
-return;
-}
+    (InputEvent.passwordLength(window.confirm("Do you want to include lowercase letters?")));
+  else
+  return;
 }
