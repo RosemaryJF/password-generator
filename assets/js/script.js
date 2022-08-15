788e09c782473be928f8d86bafccf32850950cfe
RJ Dust Puppies <109449181+RosemaryJF@users.noreply.github.com>
2022-08-15 21:54:06 +1000

    Rework of JS file, still trying to debug password push

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,54 +12,75 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function generatePassword(length) {
+function generatePassword() {
   // Arrays for character options for password
   var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"];
   var alphabetUppercase = alphabet.map(str => str.toUpperCase());
   var numericCharacters = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   var specialCharacters = ["!", "@", "#", '"', "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", "?"];
-  var passwordLength = {
-    from: 0,
-    to: 12,
-  };
-  // Concat of character options into one array
   var passwordCharacters = alphabet.concat(alphabetUppercase, numericCharacters, specialCharacters);
+  
 
   console.log(alphabet);
   console.log(alphabetUppercase);
   console.log(numericCharacters);
   console.log(specialCharacters);
   console.log(passwordCharacters);
- 
+
   //Random choosing of password characters
   
-  window.prompt(("How many charactors should it to contain?"), "")
-    if (passwordLength)
-    console.log("Password Length" + passwordLength);
+  var passwordLength = window.prompt("Enter the number of characters you want it to be (between 8-128).");
+    console.log(passwordLength);
 
-  if
-    (window.confirm("Do you want to include lowercase letters?"));
+  var lowercase = window.confirm("Do you want to include lowercase letters?");
+    console.log(lowercase);
   
-  if
-    (window.confirm("Do you want to include uppercase letters?"));
+  var uppercase = window.confirm("Do you want to include uppercase letters?");
+    console.log(uppercase);
+  
+  var numberCharacters = window.confirm("Do you want it to contain numbers?");
+    console.log(numberCharacters);
+  
+  var specialLetters = window.confirm("Do you want it to contain special characters");
+    console.log(specialLetters);
+
+  if (lowercase && uppercase && numberCharacters && specialLetters) {
+    Selection = passwordCharacters
+    console.log(Selection);
+  
+    for (var i = 0; i < passwordLength; i++) {
+      password.push(Math.floor(Math.random() * passwordLength.length));
+        }
+      }
+  // if (passwordLength === Number) {
+  //   userInput.push(Number);
+  // };
+
+  // if (lowercase === true) {
+  //   userInput.push(alphabet);
+  // };
+
+  // if (uppercase === true) {
+  //   userInput.push(alphabetUppercase);
+  // };
+
+  // if (numberCharacters === true) {
+  //   userInput.push(numericCharacters);
+  // };
+
+  // if (specialLetters === true) {
+  //   userInput.push(specialCharacters);
+  // };
+
+  // Concat of character options into one array
  
-  if
-    (window.confirm("Do you want it to contain numbers?"));
+
+
+    
+//   console.log(password);
   
-    if
-    (window.confirm("Do you want it to contain special characters"));
-    
-    
-      for (var i=8; i<length; i++) {
-      password.push(Math.floor(Math.random() * passwordCharacters.length));
-      }
-    
-    var password = " "
-      for (var i=8; i<length; i++) {
-      password += passwordCharacters.charAt(Math.floor(Math.random() * passwordCharacters.length));
-      }
-    
-    console.log(password);
-  
-  return password;
+  return Selection;
   }
+
+  // Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
